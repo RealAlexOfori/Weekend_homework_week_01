2f3ad52b8405f181ca361742e2ea5c672e39610a
user <user@users-MacBook-Pro.local>
2023-03-05 18:31:34 +0000

    First weekend homework

---
+++ start_point/src/pet_shop.py
@@ -0,0 +1,57 @@
+# WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, amount):
+    pet_shop["admin"]["total_cash"]+= amount
+    return None
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop,amount_sold):
+    pet_shop["admin"]["pets_sold"]+= amount_sold
+    return None
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop,breed):
+    found_pets = []
+    for pet in pet_shop["pets"]:
+      if pet["breed"] == breed:
+       found_pets.append(pet)
+    return found_pets
+    
+def find_pet_by_name(pet_shop,name):
+   for pet in pet_shop["pets"]:
+      if pet["name"] == name :
+       return pet 
+      
+def remove_pet_by_name(pet_shop, name):
+    for i in range(len(pet_shop["pets"])):
+        if pet_shop["pets"][i]["name"] == name:
+            del pet_shop["pets"][i]
+            break      
+    
+def add_pet_to_stock(pet_shop,new_pet):
+   pets = pet_shop["pets"]
+   pets.append(new_pet)
+   return None
+
+def get_customer_cash(customer):
+   return customer["cash"]
+
+def remove_customer_cash(customer,amount):
+    customer["cash"] -= amount
+    return None
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer,new_pet):
+    customer["pets"].append(new_pet)
+    return None
